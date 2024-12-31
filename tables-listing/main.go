58840db48d3de69f565b5aafe47bc528e65a5530
Tom Lebreux <tom.lebreux@suse.com>
2024-12-31 10:46:53 -0500

    Update tables listing

--- tables-listing/main.go
+++ tables-listing/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"net/http"
 	"os"
@@ -13,6 +14,10 @@ import (
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+)
+
+var (
+	include = flag.String("include", "Object", "One of Object, Metadata, None")
 )
 
 func must(err error) {
@@ -37,13 +42,15 @@ func (a *addQuery) RoundTrip(req *http.Request) (*http.Response, error) {
 }
 
 func main() {
+	flag.Parse()
+
 	config, err := clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
 	must(err)
 
 	setTable := func(rt http.RoundTripper) http.RoundTripper {
 		return &addQuery{
 			values: map[string]string{
-				"includeObject": "Object",
+				"includeObject": *include,
 			},
 			next: rt,
 		}
@@ -61,13 +68,33 @@ func main() {
 		Resource: "namespaces",
 	})
 
+	ctx := context.Background()
+
 	table := tablelistconvert.Client{ResourceInterface: resInt}
-	stuff, err := table.List(context.Background(), metav1.ListOptions{})
+	stuff, err := table.List(ctx, metav1.ListOptions{})
 	must(err)
 
 	bytes, err := json.Marshal(stuff)
 	must(err)
 
+	fmt.Fprintln(os.Stderr, "Initial list")
 	fmt.Println(string(bytes))
 
+	fmt.Fprintln(os.Stderr, "Starting a watch")
+	watcher, err := table.Watch(ctx, metav1.ListOptions{
+		ResourceVersion: stuff.GetResourceVersion(),
+	})
+	must(err)
+
+	for {
+		select {
+		case it := <-watcher.ResultChan():
+			bytes, err = json.Marshal(it)
+			must(err)
+
+			fmt.Println(string(bytes))
+		case <-ctx.Done():
+			return
+		}
+	}
 }
